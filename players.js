f0a8844357cd6b2cd440ba7d9edee0808b1e86f5
samkarikalan <karikalan.indo@gmail.com>
2025-12-08 18:24:59 +0900

    Update players.js

--- players.js
+++ players.js
@@ -1,1 +1,12 @@
+function showImportModal() {
+  const textarea = document.getElementById("players-textarea");
+  // Clear any entered text
+  textarea.value = "";
+  textarea.placeholder = translations[currentLang].importExample;
+  document.getElementById('importModal').style.display = 'block';
+}
 
+function hideImportModal() {
+  document.getElementById('importModal').style.display = 'none';
+  document.getElementById('players-textarea').value = '';
+}
